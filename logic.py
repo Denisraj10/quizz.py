dc35d2f89f627cf0e4d97716d7af84433d120192
Denisraj10 <denisraj2122@gmail.com>
2025-07-14 21:53:27 +0530

    logic

---
+++ logic.py
@@ -0,0 +1,23 @@
+
+
+
+line=4
+for i in range(1,line+1):
+    for j in range(1,i+1):
+        print(j, end='')
+    print()    
+
+
+
+
+
+
+
+
+i=int(input("ethana row venum"))
+for denis in range(1,i+1):
+    for who in range(1,denis+1):
+        print(who,end='')
+    print()    
+     
+    
