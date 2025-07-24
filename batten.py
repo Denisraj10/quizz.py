ae144dc6c9b2ea9559cbcc5b5f44aa5b9a12f859
Denisraj10 <denisraj2122@gmail.com>
2025-07-24 20:52:54 +0530

    batten

---
+++ batten.py
@@ -0,0 +1,29 @@
+num=7
+
+for i in range(num):
+    print("*")
+    
+    
+    
+num=7
+
+for i in range(num):
+    print("*"*i)
+    
+    
+    
+num=7
+
+for i in range(num):
+    print(" "*(num-i-1) ,"*"*i)    
+    
+    
+
+num=7
+
+for i in range(num):
+    print(" "*(num-i-1) ,"*"*(2*i+1))    
+    
+    
+    
+    
